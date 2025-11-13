5a2396c9ffe15bd7445fcc3be08d96385771abb7
MuhibChy <muhibchy90@gmail.com>
2025-11-13 17:10:55 +0000

    Optimize config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
   images: {
+    domains: ['images.unsplash.com'],
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/WebSecurity-Tech-Store' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/WebSecurity-Tech-Store' : '',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
